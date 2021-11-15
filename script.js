02e9228813072c1709c90bc3b94d830a24280905
unknown <shalinijha219999@gmail.com>
2021-11-15 17:35:31 +0530

    image filter added

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let video=document.querySelector("video");
 let record_btn_cont=document.querySelector(".record-btn-cont");
 let record_btn=document.querySelector(".record-btn");
 let timerBox=document.querySelector(".timer-cont");
+let filterLayer=document.querySelector(".filter-layer");
+let allFilters=document.querySelectorAll(".filter");
+let capture_btn_cont=document.querySelector(".capture-btn-cont");
+let capture_btn=document.querySelector(".capture-btn");
 let constraints={
     video:true,
     audio:true
@@ -9,22 +13,38 @@ let constraints={
 let recorder;
 let recordFlag=false;
 let chunks=[];
+let frames=[]
+let transparentColor;
 navigator.mediaDevices.getUserMedia(constraints).then(function(stream){
    video.srcObject=stream;
    recorder=new MediaRecorder(stream);
    recorder.addEventListener("start",(e)=>{
        console.log("started");
    //    alert("started");
+   
        chunks=[];
+       frames=[];
    })
    recorder.addEventListener("dataavailable",(e)=>{
        console.log("data available");
        chunks.push(e.data);
+       let canvas=document.createElement("canvas");
+       canvas.width=video.videoWidth;
+       canvas.height=video.videoHeight;
+       let tool=canvas.getContext('2d');
+       tool.fillStyle = transparentColor;
+       console.log("add hua : "+ transparentColor);
+       
+       tool.drawImage(video, 0, 0, canvas.width, canvas.height);
+       tool.fillRect(0, 0, canvas.width, canvas.height);
+       let frame = tool.getImageData(0, 0, canvas.width, canvas.height);
+       frames.push(frame);
       // alert("data available");
    })
    recorder.addEventListener("stop",(e)=>{
       // conversion of chunks into video
-    //  console.log("stop recording");
+     console.log("stop recording");
+     console.log(chunks);
      // console.log("length is "+chunks.length);
       let blob=new Blob(chunks,{type:"video/mp4"});
       let url=URL.createObjectURL(blob);
@@ -76,5 +96,36 @@ function stopTimer(){
     timerBox.innerText="00:00:00";
     counter=0;
 }
+
+capture_btn.addEventListener("click",(e)=>{
+   let canvas=document.createElement("canvas");
+   canvas.width=video.videoWidth;
+   canvas.height=video.videoHeight;
+   let tool=canvas.getContext('2d');
+   tool.fillStyle = transparentColor;
+   console.log("add hua : "+ transparentColor);
+   
+   tool.drawImage(video, 0, 0, canvas.width, canvas.height);
+   tool.fillRect(0, 0, canvas.width, canvas.height);
+   let dataURL = canvas.toDataURL();
+   let a=document.createElement("a");
+    a.href=dataURL;
+    a.download="image.jpg";
+    a.click();
+
+
+});
+
+allFilters.forEach(filter=>{
+filter.addEventListener("click",(e)=>{
+    console.log("clicked");
+    transparentColor=getComputedStyle(filter).getPropertyValue("background-color");
+    console.log("transparentColor : "+ transparentColor);
+    filterLayer.style.backgroundColor=transparentColor;
+})
+});
+
+
+
 
 
